166439174000c0bbd6c98ba1c3aad915ec1ef202
Nikolas Burk <nikolas.burk@gmail.com>
2024-05-16 09:48:55 +0200

    minor changes

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,17 @@ import http from "http";
 import prisma from "./lib/prisma";
 import { Server } from "socket.io";
 
-const httpServer = http.createServer();
+const httpServer = http.createServer((req, res) => {
+  // Define the routes
+  if (req.method === 'GET' && req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Server is running ...');
+  } else {
+    // Handle 404 Not Found
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
+  }
+});
 
 const io = new Server(httpServer, {
   cors: {
@@ -17,6 +27,8 @@ httpServer.listen(PORT, async () => {
   console.log(`Socket.io server is running on port ${PORT}`);
   await subscribeToPlayerUpdates(io);
 });
+
+
 
 async function subscribeToPlayerUpdates(io: Server) {
   const subscription = await prisma.player.subscribe({});
